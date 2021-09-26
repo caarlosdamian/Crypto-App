72728a5cf67f8b1284bc8051c10d0e7a68e493c0
caarlosdamian <dapecarlos@gmail.com>
2021-09-25 20:41:18 -0500

    Feat/Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components/Features/";
+import { BrowserRouter as Router } from "react-router-dom";
+import './App.css'
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main"></div>
+        <div className="footer"></div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
