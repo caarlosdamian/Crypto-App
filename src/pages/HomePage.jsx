9305c972f183720610e943647871f3cd0d0e7cc3
caarlosdamian <dapecarlos@gmail.com>
2021-09-26 20:59:39 -0500

    Feat/Add Cryptocurrencies pages funtionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ import News from "./News";
 
 const HomePage = () => {
   const { Title } = Typography;
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
   if (isFetching) return "Loading...";
