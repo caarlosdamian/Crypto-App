467c1fae56da73573027b17035cec736de991ea2
Carlos Damian <dapecarlos@gmail.com>
2022-06-28 21:50:59 -0500

    fix: fix unix timespam dates (#2)

--- src/components/Features/LineChart/LineChart.jsx
+++ src/components/Features/LineChart/LineChart.jsx
@@ -9,7 +9,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   for (let i = 0; i < coinHistory?.data.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data.history[i].price);
     coinTimestamp.push(
-      new Date(coinHistory?.data.history[i].timestamp).toLocaleDateString()
+      new Date(coinHistory?.data.history[i].timestamp * 1000).toLocaleDateString()
     );
   }
   const data = {
